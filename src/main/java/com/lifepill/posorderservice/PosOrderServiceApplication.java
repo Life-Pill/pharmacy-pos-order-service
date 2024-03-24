5727c0bda3f430c657a151cec3bb3db7b50f41d8
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-03-24 12:17:20 +0530

    initialize order microservice

---
+++ src/main/java/com/lifepill/posorderservice/PosOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lifepill.posorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosOrderServiceApplication.class, args);
+	}
+
+}
