9973b1b515aab80fe6a7deea0a430484b07310fe
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-06-03 01:56:37 +0530

    get all orders

--- src/main/java/com/lifepill/posorderservice/controller/OrderController.java
+++ src/main/java/com/lifepill/posorderservice/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.lifepill.posorderservice.controller;
 
+import com.lifepill.posorderservice.dto.OrderResponseDTO;
 import com.lifepill.posorderservice.dto.RequestOrderSaveDTO;
 import com.lifepill.posorderservice.service.OrderService;
 import com.lifepill.posorderservice.util.StandardResponse;
@@ -9,6 +10,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 /**
@@ -38,6 +41,24 @@ public class OrderController {
                         orderId
                 ),
                 HttpStatus.CREATED);
+    }
+
+    /**
+     * Retrieves all orders in the system.
+     *
+     * @return A response entity containing a list of all orders.
+     */
+    @GetMapping("/getAllOrdersWithDetails")
+    public ResponseEntity<StandardResponse> getAllOrdersWithDetails() {
+        List<OrderResponseDTO> orderResponseDTOList = orderService.getAllOrdersWithDetails();
+
+        return new ResponseEntity<>(
+                new StandardResponse(
+                        200,
+                        "All Orders Retrieved Successfully",
+                        orderResponseDTOList
+                ),
+                HttpStatus.OK);
     }
 
 }
